edf997f5ae9b143a70d53ae4bd38c191c8794c38
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-08 17:55:50 +0530

    Added toast notifications and basic styling to the editor page ,Installed react-avatar and integrated it into the editor page for user avatars

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return (
-    <div>
-      Editor Page
-    </div>
-  )
-}
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Rakesh K" },
+    { socketId: 2, username: "Jhon Doe" },
+    { socketId: 3, username: "Jhon Doe2" },
+  ]);
 
-export default EditorPage
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client
+                className="client"
+                key={client.socketId}
+                username={client.username}
+              />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn">Copy Room ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorwrap">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
