5090e32b984cd3f3fc93c6797b9963984adefef4
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-08 14:26:59 +0530

    Styled homepage with custom design and layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img className="homePageLogo" src="/code-sync.png" alt="code-pair-logo" />
+        <h4 className="mainLabel">Paste Invtitation Room ID</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="Room ID" />
+          <input type="text" className="inputBox" placeholder="USERNAME" />
 
-export default Home
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a href="" className="creteNewBtn">
+              new Room
+            </a>
+          </span>
+        </div>
+      </div>
+
+      <footer>
+        <h4>Built with ❤️ by <a href="https://github.com/dashrathshinde07" >Dashrath</a></h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
